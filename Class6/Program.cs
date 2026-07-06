ecadc0f95b7b0c73ec7071c1f44f6d5dd31206f3
DESKTOP-LKQ38HD\m <55n@naver.com>
2026-07-06 18:02:02 +0900

    feat: 20260706 수업

---
+++ Class6/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// 객체지향의 요소(Object Oriented Programming)
+// 1. 캡슐화, 2. 상속, 3. 다형성, 4. 추상화
+// 지킬 것(Class를 객체로 보자는 것)
+// 1. Single Responsibility Principle - 클래스는 하나의 책임만
+// 2. Open-Closed Principle - 기존 코드를 수정하지 않고 새 기능을 추가할 수 있게 설계
+// 3. Liskov Substitution Principle - 자식 클래스는 부모 클래스를 대체할 수 있어야 함
+// 4. Interface Segregation Principle - 클라이언트는 사용하지 않는 인터페이스(진짜 interface)에 의존하면 안 됨
+// 5. Dependency Inversion Principle - 모듈은 추상화에 의존해야 함
+
+// 접근제한자 - public, private, protected(부모의 protected는 자식만 사용 가능)
+
+// ▶클래스의 private 변수에 외부 클래스가 접근하는 법◀
+// 1. 생성자 패턴
+// 2. setter/getter 패턴
+// 3. 프로퍼티 패턴
+
+// 분류       클래스     구조체
+// 상속       O           X
+// 메서드     O           O
+// new        O           O|X
+// call       by Ref      by Val
+
+
+
+namespace Class6
+{
+    struct Student_S
+    {
+        public int age;
+        public string name;
+
+        public void veiwStudent()
+        {
+            Console.WriteLine($"이름 : {name} | 나이 : {age}");
+        }
+    }
+
+    class Student_C
+    {
+        public int age;
+        public string name;
+
+        public void veiwStudent()
+        {
+            Console.WriteLine($"이름 : {name} | 나이 : {age}");
+        }
+    }
+
+    class A
+    {
+        public Student_S student_s;
+        public Student_C student_c;
+
+        public void main()
+        {
+            student_c = new Student_C();
+            student_c.name = "김종찬";
+            student_c.age = 31;
+
+            student_s = new Student_S();
+            student_s.name = "김현호";
+            student_s.age = 33;
+        }
+
+        public void viewStudent()
+        {
+            Console.WriteLine("[Class]");
+            student_c.veiwStudent();
+
+            Console.WriteLine("[Struct]");
+            student_s.veiwStudent();
+        }
+    }
+
+    class PlayerInfo
+    {
+        public string name;
+        public int level;
+        private int hp;
+        private int mp;
+
+        // 프로퍼티 패턴 ============================================================
+        public int HP { get; private set; }
+
+
+        // 생성자 패턴 ==============================================================
+        public PlayerInfo(string name, int level, int hp, int mp)
+        {
+            this.name = name;
+            this.level = level;
+            this.hp = hp;
+            this.mp = mp;
+        }
+
+        // getter/setter 패턴 =======================================================
+        public void setHp(int hp)
+        {
+            this.hp = hp;
+        }
+
+        public int getHp(int hp)
+        {
+            return this.hp;
+        }
+
+        public void setMp(int mp)
+        {
+            this.mp = mp;
+        }
+
+        public int getMp(int mp)
+        {
+            return this.mp;
+        }
+
+
+        public void printPlayerInfo()
+        {
+            Console.WriteLine($"{name} 의 레벨은 {level} 이고 HP: {hp} | MP: {mp} 입니다");
+        }
+
+        public void demageHp(int atk)
+        {
+            Console.WriteLine("플레이어가 공격받았다!!");
+            HP -= atk;
+            Console.WriteLine($"현재 체력: {HP}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlayerInfo playerInfo = new PlayerInfo("한", 200, 100, 50);
+
+            playerInfo.name = "han";
+            playerInfo.level = 200;
+            playerInfo.setHp(10000);
+            playerInfo.setMp(5000);
+
+            playerInfo = new PlayerInfo("한", 200, 100, 50);
+
+            playerInfo.printPlayerInfo();
+
+            Console.WriteLine(playerInfo.HP); // public get, private set 즉 Program 클래스에서 HP에 대한 setter 접근 불가
+
+            Console.WriteLine("플레이어 공격");
+            playerInfo.demageHp(10);
+
+            A a = new A();
+            a.main();
+            a.viewStudent();
+
+            Console.WriteLine("[Class]");
+            a.student_c.veiwStudent();
+
+            Console.WriteLine("[Struct]");
+            a.student_s.veiwStudent();
+
+            Student_C student_c = a.student_c;
+            Student_S student_s = a.student_s;
+
+            student_c.veiwStudent();
+            student_s.veiwStudent();
+
+            student_c.name = "양성은";
+            student_c.age = 26;
+
+            student_s.name = "한덕현";
+            student_s.age = 34;
+
+            student_c.veiwStudent();
+            student_s.veiwStudent();
+
+            Console.WriteLine("______________________A 클래스 상황_______________________");
+            a.student_c.veiwStudent();
+            a.student_s.veiwStudent();
+        }
+    }
+}
---
+++ Homework8/Program.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// text rpg 만들기
+// struct Monster; class Player 존재
+// Player spec: 공격력, Hp, Mp, 닉네임, 스킬, 레벨, 경험치
+// 일반 공격과 스킬 공격이 있음
+// Mp물약과 Hp물약이 존재
+// 스킬 사용 시 Mp 소모
+// 몬스터 잡으면 경험치 얻어 레벨 업
+// 레벨 업 시 체력 마나 회복 및 필요경험치 증가
+// Monster spec: 공격력, Hp, Mp, 스킬
+// 몬스터는 3종류
+// 일정 확률로 스킬 공격
+// 게임의 종료 조건
+// 플레이어의 HP == 0 혹은 플레이어의 레벨 > 10 일 시 게임 종료
+// (이하는 기획 필요)
+// 상점 존재
+// 인벤토리 존재
+// 방식은 턴제가 좋을 듯
+
+namespace Homework8
+{
+    enum ItemList
+    {
+        GOLD, HP_POTION, MP_POTION
+    }
+
+    enum CreatureState
+    {
+        LIVING, DEAD
+    }
+
+    class Item
+    {
+        public int id { get; }
+        public string name { get; }
+        public string description { get; }
+
+        public Item(string name, string description)
+        {
+            this.id += 1;
+            this.name = name;
+            this.description = description;
+        }
+    }
+
+    class Inventory
+    {
+        private Item[] items;
+        private int inventorySize;
+
+        public Inventory(int inventorySize)
+        {
+            this.inventorySize = inventorySize;
+            this.items = new Item[this.inventorySize];
+        }
+
+        public void load(Item item)
+        {
+            for (int i = 0; i < this.items.Length; i++)
+            {
+                {
+                    if(this.items[i] != null)
+                    {
+                        this.items[i] = item;
+                        break;
+                    }
+                    
+                }
+            }
+        }
+
+        public Item pick(int index)
+        {
+            if (this.items[index] == null)
+            {
+                Console.WriteLine("해당 칸에 아이템이 없습니다.");
+            }
+
+            return this.items[index];
+        }
+    }
+
+    struct Monster
+    {
+        private string species;
+        private int number;
+        private string name;
+        private int attackPoint;
+        private int hitPoint;
+        private int magicPoint;
+        private int exp;
+        private string[] skills;
+        
+        public void createSlug(int number)
+        {
+            this.species = "달팽이";
+            this.number = number;
+            this.attackPoint = 5;
+            this.hitPoint = 20;
+            this.magicPoint = 1;
+            this.exp = 1;
+            this.skills = new string[] { "회전 박치기" };
+            this.name = this.species + this.number;
+        }
+
+        public void createSlime(int number)
+        {
+            this.species = "슬라임";
+            this.attackPoint = 10;
+            this.hitPoint = 40;
+            this.magicPoint = 5;
+            this.exp = 2;
+            this.skills = new string[] { "산성 공격" };
+            this.name = this.species + this.number;
+        }
+
+        public void createMushroom(int number)
+        {
+            this.species = "버섯";
+            this.attackPoint = 20;
+            this.hitPoint = 100;
+            this.magicPoint = 10;
+            this.exp = 5;
+            this.skills = new string[] { "바닥부수기", "회복" };
+            this.name = this.species + this.number;
+        }
+
+        public void attack()
+        {
+            Console.WriteLine($"{this.name + this.number} 의 공격");
+        }
+
+        public void getDamage(int attackPoint)
+        {
+            Console.WriteLine($"{this.name} 에게 {attackPoint} 의 피해를 줬다");
+            this.hitPoint -= attackPoint;
+
+            if (hitPoint <= 0)
+            {
+                die();
+            }
+        }
+
+        public void die()
+        {
+            Console.WriteLine($"{this.name} 은 죽었다...");
+        }
+    }
+
+    class Player
+    {
+        private string name;
+        private int attackPoint;
+        private int maxHitPoint;
+        private int hitPoint;
+        private int maxMagicPoint;
+        private int magicPoint;
+        private string[] skills;
+        private int level;
+        private int exp;
+        private int requiredExp;
+        private Inventory playerInventory;
+
+        public Player(string name)
+        {
+            this.name = name;
+            this.attackPoint = 10;
+            this.maxHitPoint = 20;
+            this.hitPoint = 20;
+            this.maxMagicPoint = 3;
+            this.magicPoint = 3;
+            this.skills = new string[] { "발차기", "방어", "회복" };
+            this.level = 1;
+            this.exp = 0;
+            this.requiredExp = 1;
+            this.playerInventory = new Inventory(10);
+        }
+
+        public void levelUp(int exceededExp)
+        {
+            this.attackPoint += 10;
+            this.hitPoint += 10;
+            this.magicPoint += 1;
+            this.level += 1;
+            this.exp = exceededExp;
+            this.requiredExp += 1;
+
+            if(exceededExp > requiredExp)
+            {
+                exceededExp -= requiredExp;
+                levelUp(exceededExp);
+            }
+        }
+
+        public void attack(Monster monster)
+        {
+            monster.
+        }
+    }
+
+    class Program
+    {
+        void GameLoop()
+        {
+            Console.WriteLine("[용사의 모험]");
+
+            while (true)
+            {
+
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.GameLoop();
+        }
+    }
+}
